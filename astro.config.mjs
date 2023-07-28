10ba4beae04e4920f3a4a25dfc42f2da95741b38
ranjana980 <ranjana20@navgurukul.org>
2023-07-28 17:54:27 +0530

    new changes

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,7 +4,6 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   integrations: [react(), tailwind()],
-  output: "server",
   plugins: [
     react({
       jsxImportSource: "@emotion/react",
